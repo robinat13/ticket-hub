2a8661c0770d45ad6ab3d2454c7b1af8af7d6b55
Robin Chauhan <robinthehood13@gmail.com>
2020-10-07 20:11:04 +0530

    Added expection handling

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,12 +1,28 @@
-import express, { json } from "express";
+import express from 'express';
+import "express-async-errors"
+import { json } from 'body-parser';
+
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
+import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
 app.use(json());
 
-app.get("/api/users/currentuser", (req, res) => {
-  res.send("Hlo there user");
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+app.all('*', async() => {
+  throw new NotFoundError();
 });
 
+app.use(errorHandler);
+
 app.listen(3000, () => {
-  console.log("Auth service started on port 3000 !");
+  console.log('Auth service started on port 3000');
 });
