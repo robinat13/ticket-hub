d1d7d7e48a336f00d7bd42539fa7819d60f0c983
Robin Chauhan <robinthehood13@gmail.com>
2020-12-01 13:54:38 +0530

    Added initial test file and jest config in pckg.json

---
+++ auth/src/test/setup.ts
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+import { MongoMemoryServer } from "mongodb-memory-server";
+import { app } from "../app";
+
+let mongo:any;
+//Hook that Runs before all the test cases start executing
+beforeAll(async () => {
+  mongo = new MongoMemoryServer();
+  const mongoURI = await mongo.getUri();
+  await mongoose.connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+});
+
+//Hook runs before each test
+beforeEach(async () => {
+    const collections = await mongoose.connection.db.collections();
+    collections.forEach(async collection => await collection.deleteMany({}));
+})
+
+//Hook that runs after all the test cases are finished
+afterAll(async () => {
+    await mongo.stop();
+    await mongoose.connection.close();
+})
