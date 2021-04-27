9e2760fbb98e3b589fa56f372a6f522da692c17b
Robin Chauhan <robinthehood13@gmail.com>
2021-04-27 20:30:27 +0530

    Finished test cases for signup route

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -11,13 +11,61 @@ it("returns a 201 on successful signup", async () => {
     .expect(201);
 });
 
-
 it("returns a 400 for an invalid email", async () => {
   return request(app)
     .post("/api/users/signup")
     .send({
       email: "test@test",
-      password: "password"
+      password: "password",
     })
     .expect(400);
-})
+});
+
+it("returns a 400 for an invalid password", async () => {
+  // return is optional if you are awaiting for the request
+  return request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test",
+      password: "pas",
+    })
+    .expect(400);
+});
+
+it("returns a 400 for an invalid input / missing fields", async () => {
+  await request(app)
+    .post("/api/users/signup")
+    .send({ email: "test@test.com" })
+    .expect(400);
+
+  await request(app)
+    .post("/api/users/signup")
+    .send({ passowrd: "gg" })
+    .expect(400);
+});
+
+it("returns a 400 for duplicate email", async () => {
+  await request(app)
+    .post("/api/users/signup")
+    .send({ email: "test@test.com", password: "password" })
+    .expect(201);
+
+  await request(app)
+    .post("/api/users/signup")
+    .send({ email: "test@test.com", password: "password" })
+    .expect(400);
+});
+
+it("checks for a cookie after successful signup", async () => {
+  const response = await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: "password",
+    })
+    .expect(201);
+    
+  // Fails by default cuz jwt session is set to true (only works for https)
+  
+  expect(response.get("Set-Cookie")).toBeDefined();
+});
