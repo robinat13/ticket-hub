9602477c2c6274b29324181cfb5d798634087b87
Robin Chauhan <robinthehood13@gmail.com>
2021-04-27 19:08:37 +0530

    Server wont start on fail db connection

--- auth/src/server.ts
+++ auth/src/server.ts
@@ -5,19 +5,18 @@ const startUp = async () => {
   if (!process.env.JWT_KEY) {
     throw new Error("Could not find JWT_KEY env variable");
   }
-  await mongoose
-    .connect("mongodb://auth-mongo-srv:27017/auth", {
+  try {
+    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth", {
       useCreateIndex: true,
       useNewUrlParser: true,
       useUnifiedTopology: true,
-    })
-    .catch((err) => {
-      console.log(err);
     });
-  console.log("Connected to db");
-
-  app.listen(3000, () => {
-    console.log("Auth service started on port 3000");
-  });
+    console.log("Connected to db");
+    app.listen(3000, () => {
+      console.log("Auth service started on port 3000");
+    });
+  } catch (err) {
+    console.log(err);
+  }
 };
 startUp();
